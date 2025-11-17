1df666043351c4d0f78a8b1d61d94744c0d283ab
LianaW04 <143361927+LianaW04@users.noreply.github.com>
2025-11-17 15:55:57 -0500

    small relatedbooks edit

--- book-details/src/components/RelatedBooks.jsx
+++ book-details/src/components/RelatedBooks.jsx
@@ -2,6 +2,7 @@ export default function RelatedBooks({ book }) {
     return (
       <div>
         <ul>
+            <p>Enjoyed {book.title}? Try these!</p>
           {book.relatedBooks.map((item, index) => (
             <li key={index}>
               <img src={item.image} alt={item.image} /> <br></br>
