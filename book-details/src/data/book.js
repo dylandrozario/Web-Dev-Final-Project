c5f89d4e338169e14bda3bdb2a0c40f8fecdbc1b
LianaW04 <143361927+LianaW04@users.noreply.github.com>
2025-11-19 12:43:13 -0500

    minor formatting

--- book-details/src/data/book.js
+++ book-details/src/data/book.js
@@ -15,16 +15,16 @@ export const book = {
     "ratings": [1,2,3,3,4,4,4,5,5,5,5,5],
     "reviews": [
         {
-            "author": "John Doe",
+            "author": "Jason",
             "rating": 5,
             "upvotes": 10,
             "comment": "I liked this book."
         },
         {
-            "author": "Jane Doe",
+            "author": "John Doe",
             "rating": 4,
             "upvotes": 20,
-            "comment": "I loved this book!"
+            "comment": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         },
         {
             "author": "Liana",
@@ -39,10 +39,10 @@ export const book = {
             "comment": "This book was alright."
         },
         {
-            "author": "Jason",
+            "author": "Sarah Smith",
             "rating": 2,
             "upvotes": 15,
-            "comment": "I disiked this book."
+            "comment": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         },
         {
             "author": "Omar",
